929e6bc306f83a34081d190356255c3a47060d71
susoterran <susoterran@naver.com>
2021-08-21 20:14:58 +0900

    admin 페이지에 Actor 추가

--- actor/admin.py
+++ actor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Actor
 
-# Register your models here.
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'name_jpn', 'name_eng', 'create_date')
+    list_filter = ('name',)
+    search_fields = ('name', 'name_jpn', 'name_eng')
+    #prepopulated_fields = {'slug': ('number',)}
